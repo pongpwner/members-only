89a4d6b35392142e6c7d26eac09f494f86bce5a3
unknown <ericsshyus@gmail.com>
2023-01-02 16:25:00 -0500

    add authController

---
+++ controllers/authController.js
@@ -0,0 +1,89 @@
+var express = require("express");
+let User = require("../models/User");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const Message = require("../models/Message");
+const { body, validationResult } = require("express-validator");
+
+exports.getSignUpForm = function (req, res, next) {
+  res.render("sign-up");
+};
+
+exports.signUp = [
+  "/sign-up",
+  body("username", "username must have 1-16 characters")
+    .trim()
+    .isLength({ min: 1, max: 16 })
+    .escape(),
+
+  body("password", "password must be betweeen 6 and 20 characters")
+    .isLength({ min: 6, max: 10 })
+    .escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty(req)) {
+      //errors found, rerender form with appropriate information
+      res.render("sign-up", {
+        errors: errors.array(),
+        username: req.body.username,
+        password: req.body.password,
+      });
+    } else {
+      // no errors is valid
+      //hash password
+      bcrypt.hash(req.body.password, 10, async function (err, hash) {
+        //check err
+        if (err) {
+          return next(err);
+        }
+        let userDetails = {
+          username: req.body.username,
+          password: hash,
+          membership: false,
+        };
+        let newUser = new User(userDetails);
+        //save user to database
+        await newUser.save((err) => {
+          if (err) {
+            return next(err);
+          }
+
+          res.redirect("/login");
+        });
+      });
+    }
+  },
+];
+exports.getLogInForm = function (req, res) {
+  res.render("login");
+};
+
+exports.logIn = [
+  body("username").escape(),
+  body("password").escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty(req)) {
+      res.render("login", {
+        password: req.body.password,
+        username: req.body.username,
+      });
+    } else {
+      next();
+    }
+  },
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+];
+exports.logOut = function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const auth_controller = require("../controllers/authController");
 var router = express.Router();
 let User = require("../models/User");
 const bcrypt = require("bcrypt");
@@ -11,95 +12,9 @@ router.get("/", async function (req, res, next) {
   res.render("index", { title: "Express", messages: messageList });
 });
 
-router.get("/sign-up", function (req, res, next) {
-  res.render("sign-up");
-});
-router.post(
-  "/sign-up",
-  body("username", "username must have 1-16 characters")
-    .trim()
-    .isLength({ min: 1, max: 16 })
-    .escape(),
-
-  body("password", "password must be betweeen 6 and 20 characters")
-    .isLength({ min: 6, max: 10 })
-    .escape(),
-
-  function (req, res, next) {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty(req)) {
-      //errors found, rerender form with appropriate information
-      res.render("sign-up", {
-        errors: errors.array(),
-        username: req.body.username,
-        password: req.body.password,
-      });
-    } else {
-      // no errors is valid
-      //hash password
-      bcrypt.hash(req.body.password, 10, async function (err, hash) {
-        //check err
-        if (err) {
-          return next(err);
-        }
-        let userDetails = {
-          username: req.body.username,
-          password: hash,
-          membership: false,
-        };
-        let newUser = new User(userDetails);
-        //save user to database
-        await newUser.save((err) => {
-          if (err) {
-            return next(err);
-          }
-
-          res.redirect("/login");
-        });
-
-        // passport.authenticate("local", {
-        //   successRedirect: "/message",
-        //   failureRedirect: "/login",
-        // });
-      });
-    }
-  }
-);
-
-router.get("/login", function (req, res) {
-  res.render("login");
-});
-router.post(
-  "/login",
-  body("username").escape(),
-
-  body("password").escape(),
-  function (req, res, next) {
-    const errors = validationResult(req);
-    console.log(errors);
-    if (!errors.isEmpty(req)) {
-      res.render("login", {
-        password: req.body.password,
-        username: req.body.username,
-      });
-    } else {
-      next();
-    }
-  },
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
-
-router.post("/logout", function (req, res, next) {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
+router.get("/sign-up", auth_controller.getSignUpForm);
+router.post("/sign-up", auth_controller.signUp);
+router.get("/login", auth_controller.getLogInForm);
+router.post("/login", auth_controller.logIn);
+router.post("/logout", auth_controller.logOut);
 module.exports = router;
